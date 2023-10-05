47ea657a4adb953e679492b5c73fc36df1b5afac
Pelumiii1 <adesokanpelumi14@gmail.com>
2023-10-05 09:46:17 +0100

    hello

---
+++ src/app/components/hero-page/hero-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-page',
+  templateUrl: './hero-page.component.html',
+  styleUrls: ['./hero-page.component.scss']
+})
+export class HeroPageComponent {
+
+  constructor() { }
+
+  handleClick() {
+    console.log('click');
+  }
+}
